832513953c21d10e2292c165e669a7a08d9fd35c
wnamen <williamjosephnamen@gmail.com>
2016-11-02 15:51:06 -0700

    added logout route

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -43,10 +43,11 @@ router.post('/login', function(req, res, next) {
       });
     })(req, res, next);
   });
-//
-// router.get('/logout', function(req, res) {
-//     req.logout();
-//     res.redirect('/');
-// });
+
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
 
 module.exports = router;
